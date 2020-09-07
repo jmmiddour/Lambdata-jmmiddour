1d64ede27c58c276569c2ef895969cc9f5386412
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2020-09-06 23:59:44 -0400

    Ran autopep, minor corrections by autopep

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dspt7_utilities_jmiddour", # the name that you will install via pip
+    name="dspt7_utilities_jmiddour",  # the name that you will install via pip
     version="0.0.2",
     author="Joanne Middour",
     author_email="joanne-middour@outlook.com",
     description="Unit 3 Utilities Testing",
     long_description=long_description,
-    long_description_content_type="text/markdown", # required if using a md file for long desc
-    #license="MIT",
+    long_description_content_type="text/markdown",
+    # required if using a md file for long desc
+    # license="MIT",
     url="https://github.com/jmmiddour/Lambdata-jmmiddour",
-    #keywords="",
-    packages=setuptools.find_packages(), # ["my_lambdata"]
+    # keywords="",
+    packages=setuptools.find_packages(),  # ["my_lambdata"]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
